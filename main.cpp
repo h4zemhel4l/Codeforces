f360ab27f796f697cf47184fa11aa616f41338e2
Hazem Helal <h4zemhel4l.gmail.com>
2022-08-31 21:49:26 +0200

    problem solving <template>

--- main.cpp
+++ main.cpp
@@ -15,38 +15,13 @@ void File(){
 }
 
 void Solve(){
-    int n; cin>>n;
-    string s; cin>>s;
-    vector<int>v;
-    for (int i = 0; i < n; ++i) {
-        int count=0;
-        int left=0,right=n-1;
-        int c1=n/2,c2=n/2;
-        for (int j = 0; j < (n/2); ++j) {
-            if((s[left]=='L')&&(c1!=0)){
-                s[left]='R';left++;c1--;break;
-            }else{
-                left++;c1--;
-            }
-            if((s[right]=='R')&&(c2!=0)){
-                s[right]='L';right--;c2--;break;
-            }else{
-                right--;c2--;
-            }
-        }
-        for (int j = 0; j < n; ++j) {
-            if(s[j]=='L')count+=j;
-            else count+=(n-j-1);
-        }
-        cout<<count<<' ';
-    }
-    cout<<endl;
+
 }
 
 int32_t main() {
     File();
     //auto Start=clock();
-    int t; cin>>t; while(t--)
+    //int t; cin>>t; while(t--)
         Solve();
     //auto End=clock();
     //cout<<End-Start<<" ms";
