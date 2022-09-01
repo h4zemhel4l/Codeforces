78d11cddf930a3db7ff1fc34707fe06456c97ece
Hazem Helal <h4zemhel4l.gmail.com>
2022-09-01 18:05:47 +0200

    problem "B. Balanced Tunnel"

--- main.cpp
+++ main.cpp
@@ -15,7 +15,33 @@ void File(){
 }
 
 void Solve(){
+    int n, a[n], b[n], first=0, second=0, counter=0;
+    map<int,bool>mp;
+    cin>>n;
+    for (int i = 0; i < n; ++i) {
+        cin>>a[i];
+        mp[a[i]]=true;
+    }
+    for (int i = 0; i < n; ++i) {
+        cin>>b[i];
+    }
 
+    for (int i = 0; i < n; ++i) {
+        if(mp[a[first]]){
+            if(a[first]==b[second]){
+                mp[b[second]]=false;
+                first++; second++;
+            }
+            else{
+                mp[b[second]]=false;
+                counter++; second++;
+            }
+
+        }else{
+            first++;
+        }
+    }
+    cout<<counter;
 }
 
 int32_t main() {
